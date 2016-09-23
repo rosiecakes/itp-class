63e623ce2da975c74d45b94e5800dbbe00112b9b
rosie shimada <naha.hana@gmail.com>
2016-09-23 18:53:01 -0400

    add unit 7 lesson 2

---
+++ unit-7-functions-intro/lesson-2-return-sum-of-numbers/tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class AddTwoNumberTest(unittest.TestCase):
+	def test_add_two_numbers(self):
+		self.assertEqual(sum_of_two_numbers(1, 2), 3)
+		self.assertEqual(sum_of_two_numbers(8, 80), 88)
+
+
