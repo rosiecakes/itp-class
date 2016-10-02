39195f9e68b380fdb29e4d29b542a4f9af9b5ccf
rosie shimada <naha.hana@gmail.com>
2016-10-02 14:05:56 -0400

    edited tests to account for py2 and 3

--- unit-4-data-types/lesson-3-return-a-float/tests.py
+++ unit-4-data-types/lesson-3-return-a-float/tests.py
@@ -4,6 +4,6 @@ import unittest
 class ReturnAFloatTest(unittest.TestCase):
     def test_return_a_float(self):
         self.assertEqual(return_float(30), 50.0)
-        self.assertEqual(return_float(13), 21.7)
+        self.assertEqual(return_float(12), 20.0)
         self.assertEqual(return_float(42), 70.0)
         self.assertEqual(return_float(60), 100.0)
